c25c11c31e9ee2bb22fb56a8f3e11e3fb940cada
R.A Borja <raborja.dev@gmail.com>
2022-09-07 14:08:30 +0800

    project done

--- script.js
+++ script.js
@@ -2,21 +2,19 @@ const button = document.querySelector('.button');
 
 const container = document.querySelector('.container.grid');
 
+container.append(...createGrid());
+
 container.addEventListener('mouseover', (event) => {
   // change background color of grid
   // squares when curose moves onto it
   event.target.style.backgroundColor = '#00203FFF';
-
-  setTimeout(() => {
-    event.target.style.backgroundColor = '';
-  }, 2000);
 });
 
 // generate grid when button is clicked
 button.onclick = createNewGrid;
 
-// handler for button's click event
-// generate a new grid based on user input
+// handler for button's click event that
+// generates a new grid based on user input
 function createNewGrid () {
   // ask for size of the grid
   let size = +prompt('Enter grid size', 16);
@@ -28,19 +26,23 @@ function createNewGrid () {
   if(size > 100) size = 100;
 
   // remove existing grid
-  if(container.innerHTML != '') container.innerHTML = '';
+  // and change grid layout
+  if(container.innerHTML != '') {
+    container.innerHTML = '';
+    container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+  } 
   
   // create grid squares
-  container.append(...createGrid());
+  container.append(...createGrid(size));
 }
 
 // create grid items
 // for grid container
-function createGrid() {
+function createGrid(size = 16) {
   // create array of divs
   const divs = [];
   
-  let numOfDiv = 16 * 16;
+  let numOfDiv = size * size;
   for(let i = 0; i < numOfDiv; i++) {
     // create div element
     const div = document.createElement('div');
